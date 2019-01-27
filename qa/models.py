2a07937a351345d6a13ba2c19b215726d0d032a4
Michael <ershovme@gmail.com>
2019-01-27 18:37:11 +0800

    Added models Question and Answer

---
+++ qa/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_at')
+
+	def popular(self):
+		return self.order_by('-rating')
+
+class Question(models.Model):
+	objects = QuestionManager()
+	title = models.CharField(max_length=50)
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	rating = models.IntegerField(default=0)
+	#author = models.ForeignKey(User)
+	likes = models.ManyToManyField(User, related_name='likes_set')
+
+	def __unicode__(self):
+		return self.title
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	question = models.ForeignKey(Question)
+	#author = models.ForeignKey(User)
+
+	def __unicode__(self):
+		return self.title
+
+
