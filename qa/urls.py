8ab39f1fb294fd43874f53b9b8514638122fc408
Michael <ershovme@gmail.com>
2019-01-28 17:50:38 +0800

    Add views New and Popular

--- qa/urls.py
+++ qa/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.test, name='test'),
-	url(r'^new/$', views.test, name='test'),
-	url(r'^popular/$', views.test, name='test'),
+	url(r'^$', views.new_questions, name='new_questions'),
+	url(r'^popular/$', views.popular_questions, name='popular_questions'),
 	url(r'^question/(?P<pk>\d+)/$', views.test, name='test'),
 	url(r'^signup/$', views.test, name='test'),
 	url(r'^login/$', views.test, name='test'),
--- qa/views.py
+++ qa/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from qa.models import Question
 
 # Create your views here.
 def test(request, *args, **kwargs):
 	return HttpResponse('Hello, world')
+
+def new_questions(request):
+	questions = Question.objects.new()
+	limit = request.GET.get('limit', 10)
+	page = request.GET.get('page', 1)
+	paginator = Paginator(questions, limit)
+	paginator.baseurl = '/?page='
+	page = paginator.page(page)
+	return render(request, 'new.html', {
+		questions: page.object_list,
+		paginator: paginator,
+		page: page,
+		})
+
+def popular_questions(request):
+	questions = Question.objects.popular()
+	limit = request.GET.get('limit', 10)
+	page = request.GET.get('page', 1)
+	paginator = Paginator(questions, limit)
+	paginator.baseurl = '/?page='
+	page = paginator.page(page)
+	return render(request, 'new.html', {
+		questions: page.object_list,
+		paginator: paginator,
+		page: page,
+		})
